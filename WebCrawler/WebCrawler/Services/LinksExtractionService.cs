35b5ea735b10d3619435112876975e2374f58312
ndm113 <39766021+ndm113@users.noreply.github.com>
2018-05-31 12:28:58 +0300

    Feature/add links extraction service (#2)
    * Added the links extraction service, as well as a test endpoint to test its work
    
    * Added a comment for the GetHtmlConentFromUrl method

---
+++ WebCrawler/WebCrawler/Services/LinksExtractionService.cs
@@ -0,0 +1,25 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebCrawler.Services
+{
+    public class LinksExtractionService : ILinksExtractionService
+    {
+        /// <summary>
+        /// Returns the complete list of links contained in the href
+        /// attributes of the a tags in the provuded html document
+        /// </summary>
+        /// <param name="htmlDocument">The html document from which to extract links</param>
+        /// <returns>An enumerable containing all link values on the page</returns>
+        public IEnumerable<string> ExtractLinksFromDocument(HtmlDocument htmlDocument) {
+            IEnumerable<HtmlNode> linkNodes = htmlDocument.DocumentNode.Descendants("a");
+            IEnumerable<string> pageLinks = linkNodes.Where(node => node.Attributes["href"] != null)
+                .Select(node => node.Attributes["href"].Value);
+
+            return pageLinks;
+        }
+    }
+}
