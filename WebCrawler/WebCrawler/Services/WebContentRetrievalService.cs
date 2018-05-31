09b70803be08b98c48c48de603f86b56a78a9c57
Dimitar Petrov <dpetrov@melontech.com>
2018-05-31 12:20:07 +0300

    Added a comment for the GetHtmlConentFromUrl method

--- WebCrawler/WebCrawler/Services/WebContentRetrievalService.cs
+++ WebCrawler/WebCrawler/Services/WebContentRetrievalService.cs
@@ -13,6 +13,12 @@ namespace WebCrawler.Services
             webContentRetriever = new HtmlWeb();
         }
 
+        /// <summary>
+        /// Retrieves the html content for a web page, 
+        /// given an url identifying it.
+        /// </summary>
+        /// <param name="webPageUrl">The url of the page to download.</param>
+        /// <returns>The html content of the url</returns>
         public HtmlDocument GetHtmlContentFromUrl (string webPageUrl) {
             HtmlDocument webPage = webContentRetriever.Load(webPageUrl);
             return webPage;
