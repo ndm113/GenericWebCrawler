2190ce1d53eed2617fa602c1932701327203de2e
ndm113 <39766021+ndm113@users.noreply.github.com>
2018-05-31 11:16:46 +0300

    Added the content retrieval service, using the HtmlAgilityPack library.
    Also added a temporary endpoint in the values controller, to verify that it retrieves content (#1)

--- WebCrawler/WebCrawler/Controllers/ValuesController.cs
+++ WebCrawler/WebCrawler/Controllers/ValuesController.cs
@@ -2,43 +2,38 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using HtmlAgilityPack;
 using Microsoft.AspNetCore.Mvc;
+using WebCrawler.Models;
+using WebCrawler.Services;
 
 namespace WebCrawler.Controllers
 {
     [Route("api/[controller]")]
     public class ValuesController : Controller
     {
+        private IWebContentRetrievalService webContentRetrievalService;
+
+        public ValuesController(IWebContentRetrievalService webContentRetrievalService) {
+            this.webContentRetrievalService = webContentRetrievalService;
+        }
         // GET api/values
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "Hello", "World" };
         }
 
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/values
-        [HttpPost]
-        public void Post([FromBody]string value)
-        {
-        }
-
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
+        [HttpPost("htmlcontent/")]
+        public string GetHtmlContent([FromBody]PageRequest htmlPageRequest) {
+            try
+            {
+                HtmlDocument htmlDocument = webContentRetrievalService.GetHtmlContentFromUrl(htmlPageRequest.Url);
+                return htmlDocument.DocumentNode.InnerHtml;
+            }
+            catch (Exception ex) {
+                return ex.Message;
+            }
+        }        
     }
 }
